8fb0c4983ece5ff05429c6df1586ab0952a47876
Luna <yanka16247@mail.com>
2026-02-17 16:16:25 +0200

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,26 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, inputValue }) => {
+  const normalizedQuery = inputValue.trim().toLowerCase();
+
+  const filteredMovies = movies.filter(movie => {
+    const titleMatch = movie.title.toLowerCase().includes(normalizedQuery);
+    const descriptionMatch = movie.description
+      .toLowerCase()
+      .includes(normalizedQuery);
+
+    return titleMatch || descriptionMatch;
+  });
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => {
+        return <MovieCard key={movie.imdbId} movie={movie} />;
+      })}
+      {filteredMovies.length === 0 && (
+        <p>{`No movies found for "${inputValue}"`}</p>
+      )}
+    </div>
+  );
+};
